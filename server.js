4fa6bdcc140eac76ab0239a7c853f1a58753a8f4
Solomon <solomonmeresa8@gmail.com>
2020-11-20 20:00:17 -0800

    modifies server.js file

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');   
+const mongoose = require('mongoose');  
+const router = require('./routes') 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(router);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -19,4 +20,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 App is connected on localhost:${PORT}`));
