2973af8fd390f9bf8b9d212470c6f59af0074b09
Solomon <solomonmeresa8@gmail.com>
2020-11-20 21:02:04 -0800

    adds Thoughts and reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,65 +1,66 @@
-// const { Schema, model, Types } = require('mongoose');
-// const dateFormat = require('../utils/dateFormat');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-// const ReplySchema = new Schema(
-//     {
-//       // set custom id to avoid confusion with parent comment _id
-//       replyId: {
-//         type: Schema.Types.ObjectId,
-//         default: () => new Types.ObjectId()
-//       },
-//       replyBody: {
-//         type: String,
-//         required: true
-//       },
-//       writtenBy: {
-//         type: String,
-//         required: true,
-//       },
-//       createdAt: {
-//         type: Date,
-//         default: Date.now,
-//         get: createdAtVal => dateFormat(createdAtVal)
-//       }
-//     },
-//     {
-//       toJSON: {
-//         getters: true
-//       }
-//     }
-//   );
-//   const CommentSchema = new Schema(
-//     {
-//       writtenBy: {
-//         type: String,
-//         required:true
+const ReactionSchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent comment _id
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+  const ThoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required:true,
+        min:1,
+        max:280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+      username:{
+        type:String,
+        required:false,
+      },
+      // use reactionSchema to validate data for a reaction
+      reactions: [ReactionSchema]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+  );
 
-//       },
-//       commentBody: {
-//         type: String,
-//         required:true
-//       },
-//       createdAt: {
-//         type: Date,
-//         default: Date.now,
-//         get: createdAtVal => dateFormat(createdAtVal)
-//       },
-//       // use ReplySchema to validate data for a reply
-//       replies: [ReplySchema]
-//     },
-//     {
-//       toJSON: {
-//         virtuals: true,
-//         getters: true
-//       },
-//       id: false
-//     }
-//   );
+  ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  })
 
-//   CommentSchema.virtual('replyCount').get(function() {
-//     return this.replies.length;
-//   })
+const Thought = model('Thought', ThoughtSchema);
 
-// const Comment = model('Comment', CommentSchema);
-
-// module.exports = Comment;
+module.exports = Thought;
